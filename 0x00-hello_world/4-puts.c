1a308b2a4faa722ea2aaaf8fa78e20c2a086d3e9
GorillaFu <jared@beguelin.com>
2020-09-17 01:24:52 +0000

    question 4 added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - puts
+ *
+ *Description: prints a line of text
+ *Returns: 0
+ */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
