61c70a659d76534b57b28e235c07ef2f542b7e3f
GorillaFu <jared@beguelin.com>
2020-09-24 03:02:23 +0000

    q10 + q9 fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers
+ * @n: first input
+ * @m: second input
+ *
+ * Return: the sum of n and m
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,41 @@
+
 #include "holberton.h"
 
 /**
- * add - adds two numbers
- * @n: first input
- * @m: second input
+ * times_table - prints times table
  *
- * Return: the sum of n and m
+ * Return: void
  */
 
-int add(int n, int m)
+void times_table(void)
 {
-	return (n + m);
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i * j < 10 && j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(i * j + '0');
+			}
+			else if (i * j > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
+			else if (j == 0)
+			{
+				_putchar(i * j + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
 }
