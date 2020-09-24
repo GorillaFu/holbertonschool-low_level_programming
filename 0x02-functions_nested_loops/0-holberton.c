94482a33b5e0158acafbf18088b3b9079f4be442
GorillaFu <jared@beguelin.com>
2020-09-24 02:24:08 +0000

    q1

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	_putchar('H');
-        _putchar('o');
-        _putchar('l');
-        _putchar('b');
-        _putchar('e');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
         _putchar('r');
         _putchar('t');
         _putchar('o');
