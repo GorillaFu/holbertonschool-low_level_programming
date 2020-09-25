3830ef2e8973793ed77f973924066f8ce225087d
GorillaFu <jared@beguelin.com>
2020-09-25 02:08:27 +0000

    q0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check if a letter is uppercase
+ * @c: input
+ * Return: true false if letter uppercase 
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
