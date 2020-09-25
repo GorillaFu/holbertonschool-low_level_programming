10a859774f6546895dcbb343762a3f0d8ba98d72
GorillaFu <jared@beguelin.com>
2020-09-25 02:31:16 +0000

    q1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check if is a number between 0 - 9
+ * @c: input
+ * Return: true or false if is a digit or not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
