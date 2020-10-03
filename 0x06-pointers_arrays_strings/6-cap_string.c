6a68a13a261f5164c339aee88e3a80ba12e49ae6
GorillaFu <jared@beguelin.com>
2020-10-03 03:50:01 +0000

    removed comments

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,6 @@ char *cap_string(char *s)
 		{
 			s[0] = s[0] - 32;
 		}
-		//check for punctuation or spaces or tabs
 		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
 		    s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
 		    s[i] == '!' || s[i] == '?' || s[i] == '(' ||
