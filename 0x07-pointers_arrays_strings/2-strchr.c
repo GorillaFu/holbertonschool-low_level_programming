c337065abdf75f9bb191fea1b83374cd47054270
GorillaFu <jared@beguelin.com>
2020-10-06 15:12:56 +0000

    updated answer q2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != c)
+	int i;
+	while (s[i] != '\0')
 	{
 		/*if *s is equal to zero, increment *s */
-		if(!*s++)
+		if (s[i] == c)
 		{
-			/**
-			 *no clue why this works????
-			 *purpose of return statement mystery
-			 */
-			return (0);
+			return (s);
 		}
+		i++;
 	}
-	return (s);
+	return (0);
 }
