15ee1b352c4e3324deb782eb84465022dd832f9d
GorillaFu <jared@beguelin.com>
2020-10-12 03:09:33 +0000

    question 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints buffer in hexa
+ * @argc: number of  command line arguments
+ * @argv: An array containing the line arguments.
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
