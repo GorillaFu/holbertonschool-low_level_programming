8dc7594fb00916822243831d5310a0c8f94c1168
GorillaFu <jared@beguelin.com>
2020-10-15 19:01:04 +0000

    fixed typo

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -47,7 +47,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		*(string + i + j) = *(s2 + j);
 	}
-	*(string + i + j + 1) = '/0';
+
+	*(string + size1 + size2 + 1) = '\0';
 
 	return (string);
 }
