47878e3401b573582118a1e2ad1855bc69b24e86
GorillaFu <jared@beguelin.com>
2020-10-15 18:09:15 +0000

    question 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * *malloc_checked - function that allocates memory using malloc
+ *
+ * @b: amount of space in pointer
+ *
+ * Return: pointer to memory or 98 if null
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+	p = malloc(b);
+
+	if(p == NULL)
+	{
+		return 98;
+	}
+	else
+		return(p);
+}
