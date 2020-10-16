80ef8788aa3fe9a90c1b50b4c1d8e951d8692c87
GorillaFu <jared@beguelin.com>
2020-10-16 16:08:01 +0000

    replaced for loop w func

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,26 @@
 #include <string.h>
 
 /**
+ * forLooperPooper - for loop separate from calloc so no void error
+ * @ptr: pointer
+ * @wrt: write
+ * @size: size
+ *
+ * Return: pointer
+ */
+
+char *forLooperPooper(char *ptr, char wrt, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = wrt;
+	}
+	return (ptr);
+}
+
+/**
  * _calloc - allocates memoy for an array using malloc
  * @nmemb: number of elements
  * @size: the amount of bytes per element
@@ -13,7 +33,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *pointer;
-	int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -27,10 +46,45 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-        for (i = 0; i < (nmemb * size); i++)
-	{
-		*(pointer + i) = '0';
-	}
+	forLooperPooper(pointer, 0, nmemb * size);
 
-	return (pointer);
+        return (pointer);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *a;
+
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Holberton");
+	strcpy(a + 9, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	free(a);
+	return (0);
 }
