14aa4e3e8bd6be245f844c66a078431abe65944d
GorillaFu <jared@beguelin.com>
2020-10-16 15:16:38 +0000

    q3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = (max - min)+1;
+	size = (max - min) + 1;
 
 	numarray = malloc(size);
 
@@ -27,7 +27,7 @@ int *array_range(int min, int max)
 	mincount = min;
 	for(i = 0; i < size; i++)
 	{
-		*(numarray +i) = mincount;
+		*(numarray + i) = mincount;
 		mincount++;
 	}
 
