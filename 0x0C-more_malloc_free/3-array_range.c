9b0eb0edec5b196c491a7cd7d63cac164d3e1f74
GorillaFu <jared@beguelin.com>
2020-10-16 15:40:35 +0000

    deleted test code

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -33,38 +33,3 @@ int *array_range(int min, int max)
 
 	return (numarray);
 }
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int *a;
-
-	a = array_range(0, 10);
-	simple_print_buffer(a, 11);
-	free(a);
-	return (0);
-}
