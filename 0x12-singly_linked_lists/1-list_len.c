4975698bdca9b2dce41b3817677ef06409b198da
GorillaFu <jared@beguelin.com>
2020-10-28 18:12:38 +0000

    list length function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns number of nodes in list
+ * @h: pointer to list
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
