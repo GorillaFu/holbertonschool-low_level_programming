f9659f5143c165e1ed3e6b45e9ee17da6f054630
GorillaFu <jared@beguelin.com>
2020-10-28 18:33:50 +0000

    add node to end function

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	list_t *traverse;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+
+	while(str[length] != '\0')
+	{
+		length++;
+	}
+	temp->len = length;
+
+	temp->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+	traverse = *head;
+	while(traverse->next)
+	{
+		traverse = traverse->next;
+	}
+
+	traverse->next = temp;
+
+	return (temp);
+}
