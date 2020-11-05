bf68077b8cebc1cbd618917bbc59cc8e79c81b2e
GorillaFu <jared@beguelin.com>
2020-11-05 01:23:22 +0000

    q8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sum the values of the list.
+ *
+ * @head:first node of the list
+ *
+ * Return: su of data.
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		count += head->n;
+		temp = head->next;
+		head = temp;
+	}
+	return (i);
+}
