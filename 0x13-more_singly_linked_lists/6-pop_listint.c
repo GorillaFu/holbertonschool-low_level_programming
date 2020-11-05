af35dc431c49679432ceb882123b5f2172a03863
GorillaFu <jared@beguelin.com>
2020-11-05 01:05:26 +0000

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - Removing the first item of a list.
+ *
+ * @head: first node
+ *
+ * Return: addres of deletec node
+ */
+int pop_listint(listint_t **head)
+{
+	int val = 0;
+	listint_t *next_node = NULL;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	next_node = (*head)->next;
+	val = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+	return (value);
+}
