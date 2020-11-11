e7d3dceeda5c1ec654d32f24f8959d2f6a67fde3
GorillaFu <jared@beguelin.com>
2020-11-11 19:22:27 +0000

    switched if statements

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -31,14 +31,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, return_write = 0;
 	int len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
 		len = _strlen(text_content);
 		return_write = write(fd, text_content, len);
