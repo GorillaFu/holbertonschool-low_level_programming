394f475bb968d6bf07242f6f04f098c64b76b296
GorillaFu <jared@beguelin.com>
2020-12-14 01:59:11 +0000

    count elements dbly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that counts doubly linked list elements
+ * @h: starting node.
+ * Return: num elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t i = 0;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
