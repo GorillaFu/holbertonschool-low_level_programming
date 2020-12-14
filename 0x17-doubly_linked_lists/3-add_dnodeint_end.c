7c732860271850bb09a0de24d3acb7140125a4bf
GorillaFu <jared@beguelin.com>
2020-12-14 17:44:45 +0000

    main file

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	*temp = *head;
+	temp = *head;
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
