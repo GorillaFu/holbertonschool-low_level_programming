698ee005c4602eeec3b30b5969961cf1a5f2d90d
GorillaFu <jared@beguelin.com>
2020-12-14 20:34:23 +0000

    betty fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,10 +14,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (head == NULL)
 		return (-1);
-
 	temp = *head;
 	last = *head;
-
 	if (temp == NULL || last == NULL)
 		return(-1);
 	if (index == 0)
