b16ccc113dcf04d7ce0dc90090bb4e7ef75bc917
GorillaFu <jared@beguelin.com>
2020-12-29 19:19:01 +0000

    returns int to the power of another int

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns an int to the power of another int
+ * @x: base
+ * @y: exponent
+ *
+ * Return: x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, (y - 1));
+}
