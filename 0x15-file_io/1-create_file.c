d716483994a437e7747ff0d3b8631243f31880ce
GorillaFu <jared@beguelin.com>
2020-12-29 00:12:55 +0000

    changed var name

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, return_write;
+	int fd, ret_write;
 	int i = 0;
 
 	if (filename == NULL)
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[i] != '\0')
 		i++;
-	return_write = write(fd, text_content, i);
-	if (return_write == -1)
+	ret_write = write(fd, text_content, i);
+	if (ret_write == -1)
 		return (-1);
 	return (1);
 }
