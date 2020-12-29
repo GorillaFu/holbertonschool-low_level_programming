70159bb1a62b142ec6734c9d0b9f3fa5f90166de
GorillaFu <jared@beguelin.com>
2020-12-29 05:58:18 +0000

    added return for idx oob

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	while (i < idx)
 	{
+		if (*head->next == NULL)
+			return (NULL);
 		node = (*head)->next;
 		*head = node;
 		++i;
