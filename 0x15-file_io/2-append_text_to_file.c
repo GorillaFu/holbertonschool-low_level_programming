6774f5aa4a105a3c131669a8375f1e60515751eb
GorillaFu <jared@beguelin.com>
2020-12-29 00:28:38 +0000

    make wronly

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,9 +11,9 @@
 
 int _strlen(char *s)
 {
-	int length = 0;
+	int length;
 
-	while (s[length])
+	while(s[length])
 		length++;
 	return (length);
 }
@@ -34,7 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
