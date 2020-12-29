f8db1eb026ce4878b55a62cee6416f8ccc58b45f
GorillaFu <jared@beguelin.com>
2020-12-29 06:02:47 +0000

    fixed typo

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		tep = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 		return (1);
