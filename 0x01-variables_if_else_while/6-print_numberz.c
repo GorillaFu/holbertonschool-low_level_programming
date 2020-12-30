f7a0f9baac9c8ad3cbb4d37a72d183ccefcd9ec0
GorillaFu <jared@beguelin.com>
2020-12-30 01:01:59 +0000

    print decimal digits w putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print decimal digits using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
