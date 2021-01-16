c4a6bd508ae1e23898437e6167b0f48a4bd80520
GorillaFu <jared@beguelin.com>
2021-01-16 03:48:24 +0000

    fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,7 +20,7 @@ void hash_table_delete(hash_table_t *ht)
 			while (array)
 			{
 				free(array->key);
-				free(array->value);
+				free(array->value); /*empty string???*/
 				temp = array;
 				array = array->next;
 				free(temp);
