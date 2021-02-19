be3c65ef78a5d4f38142b6331fb05998efb88735
GorillaFu <jared@beguelin.com>
2021-02-19 20:44:28 +0000

    added validators

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,15 +9,18 @@ def island_perimeter(grid):
     perimeter = 0
     if grid:
         if len(grid) > 0:
-            for r in range(len(grid)):
-                for c in range(len(grid[r])):
-                    if grid[r][c] == 1:
-                        if grid[r - 1][c] == 0:
-                            perimeter += 1
-                        if grid[r + 1][c] == 0:
-                            perimeter += 1
-                        if grid[r][c-1] == 0:
-                            perimeter += 1
-                        if grid[r][c+1] == 0:
-                            perimeter += 1
+            if all(type(element) == list for element in grid):
+                rowlen = len(grid[0])
+                if all(len(element) == rowlen for element in grid):
+                    for r in range(len(grid)):
+                        for c in range(len(grid[r])):
+                            if grid[r][c] == 1:
+                                if grid[r - 1][c] == 0:
+                                    perimeter += 1
+                                if grid[r + 1][c] == 0:
+                                    perimeter += 1
+                                if grid[r][c-1] == 0:
+                                    perimeter += 1
+                                if grid[r][c+1] == 0:
+                                    perimeter += 1
     return perimeter
