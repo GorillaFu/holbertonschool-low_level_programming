f6bb25960078ac431df2e5b9a74ca3c14c873875
GorillaFu <jared@beguelin.com>
2021-02-19 19:53:53 +0000

    check island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that returns perimeter of island
+"""
+def island_perimeter(grid):
+    if grid:
+        perimeter = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == 1:
+                    if grid[r - 1][c] == 0:
+                        perimeter += 1
+                    if grid[r + 1][c] == 0:
+                        perimeter += 1
+                    if grid[r][c-1] == 0:
+                        perimeter += 1
+                    if grid[r][c+1] == 0:
+                        perimeter += 1
+        return perimeter
