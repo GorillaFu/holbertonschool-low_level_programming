7217d7d097fe5323410c3eb81571d29e781a5596
GorillaFu <jared@beguelin.com>
2021-02-19 20:29:05 +0000

    added hecker

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,17 @@ def island_perimeter(grid):
     """calculates island perimeter"""
 
     if grid:
-        perimeter = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 1:
-                    if grid[r - 1][c] == 0:
-                        perimeter += 1
-                    if grid[r + 1][c] == 0:
-                        perimeter += 1
-                    if grid[r][c-1] == 0:
-                        perimeter += 1
-                    if grid[r][c+1] == 0:
-                        perimeter += 1
-        return perimeter
+        if len(grid) > 0:
+            perimeter = 0
+            for r in range(len(grid)):
+                for c in range(len(grid[r])):
+                    if grid[r][c] == 1:
+                        if grid[r - 1][c] == 0:
+                            perimeter += 1
+                        if grid[r + 1][c] == 0:
+                            perimeter += 1
+                        if grid[r][c-1] == 0:
+                            perimeter += 1
+                        if grid[r][c+1] == 0:
+                            perimeter += 1
+            return perimeter
