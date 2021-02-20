9063b80eccc3d4aeaec895dbe5131a05d1c8c57f
GorillaFu <jared@beguelin.com>
2021-02-20 02:42:37 +0000

    added edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,15 +12,16 @@ def island_perimeter(grid):
             if all(type(element) == list for element in grid):
                 rowlen = len(grid[0])
                 if all(len(element) == rowlen for element in grid):
+                    rowlen = rowlen - 1
                     for r in range(len(grid)):
                         for c in range(len(grid[r])):
                             if grid[r][c] == 1:
-                                if grid[r - 1][c] == 0:
+                                if r == 0 or grid[r - 1][c] == 0:
                                     perimeter += 1
-                                if grid[r + 1][c] == 0:
+                                if r == rowlen or grid[r + 1][c] == 0:
                                     perimeter += 1
-                                if grid[r][c-1] == 0:
+                                if c == 0 or grid[r][c-1] == 0:
                                     perimeter += 1
-                                if grid[r][c+1] == 0:
+                                if c == len(grid[r]) -1 or grid[r][c+1] == 0:
                                     perimeter += 1
     return perimeter
